241edb4365e5bc339447f68bd6016da3636583a9
Areeb ul haq Mirza <auhmirza02@gmail.com>
2023-08-03 16:10:55 +0100

    Add command line interface for game

--- file.js
+++ file.js
@@ -5,8 +5,7 @@ const fs=require("fs")
 function readBingoBoard(path){
     const content = fs.readFileSync(path,'utf-8')
     const rows= content.trim().split('\n')
-    console.log(content,rows)
-    const bingoBoard = rows.map((row) => {
+       const bingoBoard = rows.map((row) => {
         return row.split(/\s+/).map((number) => {
             const parsedInt = parseInt(number)
             if (!isNaN(parsedInt)) {
--- main.js
+++ main.js
@@ -1,7 +1,49 @@
-function main(){
-    
+const readline = require('readline')
+const { game,fastestBingoBoard } = require("./game")
+const {readBingoBoard,readInputList}=require('./file')
 
+async function main(){
+    
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    })
+
+    try {
+        rl.question('Enter the name of your input file: ', (inputFile) => {
+            rl.question('Enter the name of your board file. If there is more than one file, seperate them by commas: ', (boardFiles) => {
+                let inputList = readInputList(`./textfiles/${inputFile}`)
+                let boardFilesArray = boardFiles.split(',')
+                console.log(inputList)
+                console.log(boardFilesArray)
+                let boards = []
+                for (let i = 0; i <  boardFilesArray.length; i++) {
+                    let file = boardFilesArray[i]
+                    boards.push(readBingoBoard(`./textfiles/${file}`))
+                }
+                if(boards.length == 1){
+                    console.log("here2")
+                    let result = game(inputList,boards[0])
+                    if (result.bingo == true){
+                        console.log("!!!BINGO!!! YOU WIN")
+                    }else{
+                        console.log("YOU LOSE!!! TRY AGAIN!!!")
+                    }
+                }else{
+                    console.log("here3")
+                    let result = fastestBingoBoard(inputList,boards)
+                    console.log(`HERE IS THE FASTEST BOARD `)
+                    console.log(`${result.board}`)
+                }
+                
+
+                rl.close()
+            })
+        })
+    } catch (err) {
+        console.error('An error occurred: ', err)
+        rl.close()
+    }
  
 }
-
-
+main();
