616ce4f7590dd2597957e6d4aa3c0fcb3259d9eb
Areeb ul haq Mirza <auhmirza02@gmail.com>
2023-08-03 13:03:25 +0100

    refactor readBingoBoard

--- file.js
+++ file.js
@@ -3,18 +3,24 @@ const fs=require("fs")
 
 
 function readBingoBoard(path){
-    const content = fs.readFileSync
+    const content = fs.readFileSync(path,'utf-8')
     const rows= content.trim().split('\n')
     console.log(content,rows)
     const bingoBoard = rows.map((row) => {
         return row.split(/\s+/).map((number) => {
-            return parseInt(number);
-        });
-      });
+            const parsedInt = parseInt(number)
+            if (!isNaN(parsedInt)) {
+              return parsedInt
+            }
+          }).filter((number) => number !== undefined);
+
+        
+      })
     return bingoBoard
 
 }
-console.log(readBingoBoard('./board.txt'))
+
+
 module.exports={
     readBingoBoard,
 }
